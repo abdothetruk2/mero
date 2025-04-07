517370e6b54e4e1616c47dd7d6c314e98db3971b
abdothetruk3 <abdokhaterel@gmail.com>
2025-04-07 09:53:24 +0200

    Update socket.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = "https://fly-patient-firefly-4760-production.up.railway.app/"
+const SOCKET_URL = "https://fly-patient-firefly-4760-production.up.railway.app/socket.io"
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
