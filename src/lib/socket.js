3b463b1926217c60fe8c4f73954b04a62f5ff2bd
abdothetruk3 <abdokhaterel@gmail.com>
2025-04-08 02:33:27 +0200

    Update socket.js

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,12 +1,11 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = "https://mero2.up.railway.app"
+const SOCKET_URL = "mero2.up.railway.app"
 
 export const socket = io(SOCKET_URL, {
   path: '/socket.io/',
   transports: ['websocket','polling'],
-  upgrade: true,
-  secure: true
+  secure: false
 });
 
 export const connectSocket = (username) => {
